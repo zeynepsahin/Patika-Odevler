4928fc7a88024ed919f41ed5d58ad001f3d9994d
zeynepsahin <zeynepsahin.bm@gmail.com>
2023-02-23 15:29:51 +0300

    odev10 yüklendi

---
+++ SQL/ODEV10.sql
@@ -0,0 +1,12 @@
+--1
+
+SELECT city.city, country.country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+--2
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer 
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+--3
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer 
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
